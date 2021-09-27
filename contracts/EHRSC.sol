2d1d9ee3ae0b39dac1cb4a6f7b4a4690a0e18b5c
kuroza <as.siddiqeen@gmail.com>
2021-09-27 18:06:40 +0800

    Add function for a doctor to request documents

--- contracts/EHRSC.sol
+++ contracts/EHRSC.sol
@@ -3,32 +3,40 @@ pragma solidity >=0.4.22 <0.8.0;
 
 contract EHRSC {
     struct Person {
+        address personAddress;
         string name;
         bool registered;
         bool verifiedDoctor;
         bool verifiedPatient;
+        uint256 requestCount;
         mapping(uint256 => Request) requests;
     }
 
     struct Request {
         address requester;
-        uint256 documentNumber;
+        uint256 bundleNumber;
     }
 
     mapping(address => Person) public persons;
 
-    constructor() public {}
-
     function registerDoctor(string memory _name) public payable {
         require(!persons[msg.sender].registered, "Person already registered");
-
-        persons[msg.sender] = Person(_name, true, true, false);
+        Person storage doctor = persons[msg.sender];
+        doctor.personAddress = msg.sender;
+        doctor.name = _name;
+        doctor.registered = true;
+        doctor.verifiedDoctor = true;
+        doctor.verifiedPatient = false;
     }
 
     function registerPatient(string memory _name) public payable {
         require(!persons[msg.sender].registered);
-
-        persons[msg.sender] = Person(_name, true, false, true);
+        Person storage patient = persons[msg.sender];
+        patient.personAddress = msg.sender;
+        patient.name = _name;
+        patient.registered = true;
+        patient.verifiedDoctor = false;
+        patient.verifiedPatient = true;
     }
 
     function getName() public view returns (string memory) {
@@ -43,19 +51,29 @@ contract EHRSC {
         return persons[msg.sender].verifiedPatient;
     }
 
-    // function requestDocuments(address patientAddress, uint256 documentNumber)
-    //     public
-    //     payable
-    // {
-    //     require(persons[msg.sender].verifiedDoctor);
-    //     require(persons[patientAddress].verifiedPatient); // TODO: document must exist
+    function requestDocuments(address patientAddress, uint256 bundleNumber)
+        public
+        payable
+    {
+        require(persons[msg.sender].verifiedDoctor);
+        // require(persons[patientAddress].verifiedPatient); // TODO: document must exist
 
-    //     Request memory request;
-    //     request.requester = msg.sender;
-    //     request.documentNumber = documentNumber;
+        Request memory request;
+        request.requester = msg.sender;
+        request.bundleNumber = bundleNumber;
 
-    //     persons[patientAddress].requests = request;
-    // }
+        uint256 count = persons[patientAddress].requestCount;
+        count++;
+        persons[patientAddress].requests[count] = request;
+        persons[patientAddress].requestCount = count;
+    }
 
-    // TODO: patient documents
+    function checkPatientDocumentRequests() public view returns (uint256) {
+        require(persons[msg.sender].verifiedPatient, "Must be a patient");
+        return persons[msg.sender].requestCount; // test checking how many requests are there
+        // TODO: check the contains of requests object array
+    }
+
+    // TODO: function: RA verifies patient and doctor
+    // TODO: function: Patient allow access to doctor or send re-encryption keys to NuCypher network
 }
